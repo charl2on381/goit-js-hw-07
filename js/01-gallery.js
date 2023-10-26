0c76e88b38cbf302cef6a5c0d9ab300ee106636b
charl2on381 <charl2on381@gmail.com>
2023-10-26 22:36:36 +0800

    Update Sir Marvin Code

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -65,33 +65,6 @@
 
 
 //sir Marvin Code
-
-
-import { galleryItems } from "./gallery-items.js";
-
-  // Change code below this line
-
-  const galleryList = document.querySelector(".gallery");
-
-  const createGallery = (el) => {
-    return el.map(({ preview, original, description }) => {
-      return `
-      <li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-      />
-    </a>
-  </li>
-
-  `;
-    });
-  };
-
-  console.log(galleryItems);
   import { galleryItems } from "./gallery-items.js";
 
   // Change code below this line
